22ff324d029df39fc0e8d0b33a5275dc2c31512a
jamil neto zabotto <jamilzabneto@hotmail.com>
2019-08-12 18:58:05 -0300

    Validation Exception

--- src/GUI/DepartmentFormController.java
+++ src/GUI/DepartmentFormController.java
@@ -3,7 +3,9 @@ package GUI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import GUI.listeners.DataChangeListener;
 import GUI.util.Alerts;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationExceptions;
 import model.service.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -72,6 +75,8 @@ public class DepartmentFormController implements Initializable {
 			Utils.currentStage(event).close();
 		}catch(DbException e) {
 			Alerts.showAlert("ERRO SAVING OBJECT", null, e.getMessage(), AlertType.ERROR);
+		}catch(ValidationExceptions e) {
+			setErrorsMessages(e.getErrors());
 		}
 		
 		
@@ -86,8 +91,21 @@ public class DepartmentFormController implements Initializable {
 
 	private Department getFormData() {
 		Department obj = new Department();
+		
+		ValidationExceptions exception = new ValidationExceptions("Validation ERROR");
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(txtname.getText() == null || txtname.getText().trim().equals("")) {
+			exception.addErrors("name", "Field can't be empty");
+		}
+		
 		obj.setName(txtname.getText());
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		return obj;
 	}
 
@@ -116,6 +134,13 @@ public class DepartmentFormController implements Initializable {
 		
 		txtId.setText(String.valueOf(entity.getId()));
 		txtname.setText(entity.getName());
+	}
+	
+	private void setErrorsMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if(fields.contains("name")) {
+			lblErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
